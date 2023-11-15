b2b9d4720995563cda29cea89193e67af671f7b8
Sandeep Shrestha <sandeepshrestha288@gmail.com>
2023-11-15 23:44:01 +0545

    Dark mode added in About page using useState

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
@@ -22,17 +22,17 @@ const Navbar = (props) => {
               </a>
             </li>
           </ul>
-          <form className="d-flex" role="search">
+          {/* <form className="d-flex" role="search">
             <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
+            <button className="btn btn-outline-primary" type="submit">
               Search
             </button>
-          </form>
+          </form> */}
         </div>
       </div>
     </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,28 +2,54 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
-    console.log("Uppercase");
     let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleClearClick = () => {
+    let newText = "";
     setText(newText);
   };
   const handleonChange = (event) => {
     setText(event.target.value);
   };
-  const [text, setText] = useState("Enter your text");
+  const [text, setText] = useState("");
   return (
     <>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="mybox"
-          rows="8"
-          value={text}
-          onChange={handleonChange}
-        ></textarea>
-        <button className="btn btn-primary" onClick={handleUpClick}>
-          Convert to UpperCase
-        </button>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="mybox"
+            rows="8"
+            value={text}
+            onChange={handleonChange}
+          ></textarea>
+          <div className="my-2">
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+              Convert to UpperCase
+            </button>
+            <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+              Convert to Lowercase
+            </button>
+            <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+              Clear Text
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container my-3">
+        <h2>Your Text Summary :</h2>
+        <p>
+          {text.split(" ").length} Words and {text.length} Characters
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes requires to read.</p>
+        <h2 className="my-3">Preview :</h2>
+        <p>{text}</p>
       </div>
     </>
   );
